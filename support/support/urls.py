60bd82d3b0580d2ba82aa8b4d93541a06ed21274
OleksandrUzhva <oleksandruzhva@gmail.com>
2024-03-17 16:08:56 +0100

    :pushpin: Added for code quality but without fake8 becouse url very long

--- support/support/urls.py
+++ support/support/urls.py
@@ -1,16 +1,16 @@
 import httpx
-import json
 from django.contrib import admin
+from django.http import HttpRequest, JsonResponse
 from django.urls import path
-from django.http import HttpRequest, JsonResponse, HttpResponseNotAllowed
 from django.views.decorators.csrf import csrf_exempt
+
 
 @csrf_exempt
 async def currency(request: HttpRequest) -> JsonResponse:
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.POST
-        currency_from = data.get('from_currency')
-        currency_to = data.get('to_currency')
+        currency_from = data.get("from_currency")
+        currency_to = data.get("to_currency")
 
         url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={currency_from}&to_currency={currency_to}&apikey=D5G28HSZ0FWYD5WQ"
 
@@ -19,9 +19,9 @@ async def currency(request: HttpRequest) -> JsonResponse:
             rate = response.json()
 
         return JsonResponse({"rate": rate})
-    
-    
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("currency", view=currency)
+    path("currency", view=currency),
 ]
