96c2a8d5679d1fa098c70f82a3d2ad9641d7f1bf
OleksandrUzhva <oleksandruzhva@gmail.com>
2024-04-07 17:17:48 +0200

    Added for action

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView  # noqa
+# from rest_framework_simplejwt.views import TokenObtainPairView  # noqa
 from rest_framework_simplejwt.views import token_obtain_pair
 
 from issues.api import get_issues, post_issues, retrieve_issues
